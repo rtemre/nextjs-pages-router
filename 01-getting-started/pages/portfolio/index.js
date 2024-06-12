5827c3b0c509dc2f2d0734a16d0dee24b6e70c2a
Raman Temre <ramantemre2110@gmail.com>
2024-06-12 11:23:01 +0530

    Work with nested pages & routes

---
+++ 01-getting-started/pages/portfolio/index.js
@@ -0,0 +1,4 @@
+function PortfolioPage(params) {
+  return <h1>Portfolio Page</h1>;
+}
+export default PortfolioPage;
---
+++ 01-getting-started/pages/portfolio/list.js
@@ -0,0 +1,4 @@
+function ListPage(params) {
+  return <h1>List Page</h1>;
+}
+export default ListPage;
